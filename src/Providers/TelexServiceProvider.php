73a42ade2ab058826ac08655bf1834750f48a41d
Chibuzor <nwalorc@gmail.com>
2019-09-25 11:14:59 +0100

    Initial commit

---
+++ src/Providers/TelexServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Telex\Providers;
+
+use Telex\TelexClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class TelexServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->singleton('Telex', function ($app) {
+            $organizationKey = config('services.telex.organization_key');
+
+            return new TelexClient($organizationKey);
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
