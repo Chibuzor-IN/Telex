178c7e393357b710df1612e8bcf76ef3ea67fd16
Chibuzor Nwalor <nwalorc@gmail.com>
2019-12-10 12:03:42 +0100

    Added support for adding extra metadata keys

--- src/TelexClient.php
+++ src/TelexClient.php
@@ -25,23 +25,27 @@ class TelexClient
         ]);
     }
 
-    public function sendEvent(string $eventName, array $customer, array $placeholderData, array $tagData=[], string $receiverEmail=null)
+    public function sendEvent(string $eventName, array $customer, array $placeholderData, array $tagData=[], string $receiverEmail=null, array $metadataOptions=[])
     {
         $url = "{$this->telexDomain}/api/events";
-        $payload = [
-            'json' => [
-                'customer' => $customer,
-                'metadata' => [
-                    'placeholders' => $placeholderData
-                ],
-                'event_type' => $eventName,
-                'tag_data' => $tagData,
-            ]
+        $data = [
+            'customer' => $customer,
+            'metadata' => [
+                'placeholders' => $placeholderData
+            ],
+            'event_type' => $eventName,
+            'tag_data' => $tagData,
         ];
 
         if (!empty($receiverEmail)) {
-            $payload['json']['metadata']['receiver_email'] = $receiverEmail;
+            $data['metadata']['receiver_email'] = $receiverEmail;
         }
+
+        $data = array_merge($data['metadata'], $metadataOptions);
+
+        $payload = [
+            'json' => $data
+        ];
 
         return $this->client->request('POST', $url, $payload);
     }
